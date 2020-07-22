7ff039b5da594f8cbded8db5ba9e7c73c688b088
Dillon Long <dplong@crimson.ua.edu>
2020-07-22 08:44:35 -0700

    Completed the 00a and 00b files.

--- data-access-layer/00b-seed.sql
+++ data-access-layer/00b-seed.sql
@@ -8,7 +8,7 @@
 -- user.
 
 -- YOUR CODE HERE
-
+SET ROLE recipe_box_app
 
 
 
@@ -25,7 +25,11 @@
 -- | updated     | TIMESTAMP    | NOT NULL, DEFAULT CURRENT_TIMESTAMP |
 
 -- YOUR CODE HERE
-
+CREATE TABLE recipes
+    (id SERIAL PRIMARY KEY,
+     title VARCHAR(200) NOT NULL,
+     created TIMESTAMP NOT NULL, DEFAULT CURRENT_TIMESTAMP,
+     updated TIMESTAMP NOT NULL, DEFAULT CURRENT_TIMESTAMP);
 
 
 
@@ -47,7 +51,12 @@
 -- | recipe_id     | INTEGER     | FK, NOT NULL |
 
 -- YOUR CODE HERE
-
+CREATE TABLE instructions
+  (id SERIAL PRIMARY KEY,
+   specification TEXT NOT NULL,
+   list_order INTEGER NOT NULL,
+   recipe_id INTEGER NOT NULL,
+   FOREIGN KEY (recipe_id) REFERENCES recipes (id));
 
 
 
@@ -64,7 +73,9 @@
 
 -- YOUR CODE HERE
 
-
+CREATE TABLE unit_of_measure
+  (id SERIAL PRIMARY KEY,
+   name VARCHAR(20) NOT NULL);
 
 
 
@@ -86,7 +97,14 @@
 -- | recipe_id          | INTEGER       | FK, NOT NULL |
 
 -- YOUR CODE HERE
-
+ CREATE TABLE ingredients
+  (id SERIAL PRIMARY KEY,
+   amount NUMERIC(5, 2) NOT NULL,
+   unit_of_measure_id INTEGER NOT NULL,
+   food_stuff VARCHAR(500) NOT NULL,
+   recipe_id INTEGER NOT NULL,
+   FOREIGN KEY (unit_of_measure_id) REFERENCES unit_of_measure (id),
+   FOREIGN KEY (recipe_id) REFERENCES recipes (id));
 
 
 
